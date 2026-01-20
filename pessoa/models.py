54f61ce2541ab7db5e67a0a4d2dc5986496763bb
Ésley Nathan <31036923+Esleynathan@users.noreply.github.com>
2026-01-20 12:38:32 -0300

    feat(model): criar entidade Pessoa e migrações

---
+++ pessoa/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Pessoa(models.Model):
+    SEXO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=14, unique=True)
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    altura = models.FloatField()
+    peso = models.FloatField()
+
+    class Meta:
+        db_table = 'pessoa'
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+
+    def __str__(self):
+        return self.nome
