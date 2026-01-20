c2094530ca81aa0808ed54cce6ef2e96242054e6
Ésley Nathan <31036923+Esleynathan@users.noreply.github.com>
2026-01-20 12:39:24 -0300

    feat(service): criar camada de serviço para orquestração

---
+++ pessoa/services.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+from pessoa.models import Pessoa
+from pessoa.dtos import PessoaDTO
+from pessoa.tasks import PessoaTask
+
+
+class PessoaService:    
+    def __init__(self):
+        self.task = PessoaTask()
+
+    def incluir(self, dto: PessoaDTO) -> Pessoa:
+        return self.task.incluir(dto)
+
+    def alterar(self, dto: PessoaDTO) -> Pessoa:
+        return self.task.alterar(dto)
+
+    def excluir(self, id: int) -> bool:
+        return self.task.excluir(id)
+
+    def pesquisar(self, id: Optional[int] = None) -> List[Pessoa]:
+        return self.task.pesquisar(id)
+
+    def calcular_peso_ideal(self, id: int) -> float:
+        return self.task.calcular_peso_ideal(id)
